b231cb6e5cc199e2e19a0a95d9e64409010f2536
Guilherme Santos <guisgb13@gmail.com>
2012-12-03 21:36:04 -0600

    Cleanup - removing old cs638 project

--- cs638-game-android/gen/com/cs638/game/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.cs638.game;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- cs638-game/src/com/cs638/game/Explosion.java
+++
@@ -1,8 +0,0 @@
-package com.cs638.game;
-
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public abstract class Explosion {
-
-	public abstract boolean draw(SpriteBatch batch);	
-}
